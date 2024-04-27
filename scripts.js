5a0c999d08a4e32bde23d0ec9aa25224888120af
Cees Voesenek <cjvoesenek@gmail.com>
2024-04-27 18:55:25 +0200

    Abstract block schedule to class

--- scripts.js
+++ scripts.js
@@ -4,30 +4,37 @@ let enabledStageIds = [];
 const blockHeightMinutes = 30;
 const blockHeight = 100;
 
+// Festival schedule class.
+//
+// This contains a list of stages and a schedule, where the schedule is a list
+// of days, each with a list of events for each stage.
 class Schedule {
+  #stages;
+  #schedule;
+
   constructor(stages, schedule) {
-    this.stages = stages;
-    this.schedule = schedule;
+    this.#stages = stages;
+    this.#schedule = schedule;
   }
 
   getDayIds() {
-    return this.schedule.map((day) => day.id);
+    return this.#schedule.map((day) => day.id);
   }
 
   getStageIds(dayId) {
     if (dayId === undefined) {
-      return this.stages.map((stage) => stage.id);
+      return this.#stages.map((stage) => stage.id);
     } else {
-      return Object.keys(this.schedule.find((day) => day.id === dayId).events);
+      return Object.keys(this.#schedule.find((day) => day.id === dayId).events);
     }
   }
 
   getStage(stageId) {
-    return this.stages.find((stage) => stage.id === stageId);
+    return this.#stages.find((stage) => stage.id === stageId);
   }
 
   getEvents(dayId, stageId) {
-    return this.schedule.find((day) => day.id === dayId).events[stageId];
+    return this.#schedule.find((day) => day.id === dayId).events[stageId];
   }
 
   static async fetch(url) {
@@ -37,6 +44,136 @@ class Schedule {
     const schedule = data.schedule;
     return new Schedule(stages, schedule);
   }
+}
+
+// Block schedule class.
+//
+// This class represents a block schedule for a single stage on a single day as
+// an SVG element.
+class BlockSchedule {
+  // The height of each block in "minutes", to determine the aspect ratio of the
+  // blocks.
+  static #BLOCK_HEIGHT_MINUTES = 30;
+  // The height of the SVG (i.e. height of the block) in pixels.
+  static #HEIGHT = 100;
+  // The stroke colour for the blocks, mainly useful when events are directly
+  // adjacent to each other.
+  static #STROKE_COLOUR = "white";
+  // The stroke with for the blocks.
+  static #STROKE_WIDTH = 0.5;
+
+  #svg;
+
+  constructor(svg) {
+    this.#svg = svg;
+  }
+
+  get svg() {
+    return this.#svg;
+  }
+
+  // Create a block schedule from a schedule object for a specific day and
+  // stage.
+  static fromSchedule(schedule, dayId, stageId) {
+    const events = schedule.getEvents(dayId, stageId);
+
+    // Colour for this block schedule (i.e. the colour for the current stage).
+    const stage = schedule.getStage(stageId);
+    const stageColour = stage.colour;
+
+    // Create a root SVG element for each block schedule, do not automatically
+    // stretch the SVG to preserve the aspect ratio; we take care of this
+    // manually.
+    const svg = createSvgElement("svg", {
+      preserveAspectRatio: "none",
+    });
+
+    // Create groups for blocks and text.
+    const gBlocks = createSvgElement("g", {});
+    const gText = createSvgElement("g", {});
+    svg.appendChild(gBlocks);
+    svg.appendChild(gText);
+
+    // Create blocks for each event, coordinates are in minutes from 00:00
+    // today. Note: any events after midnight (i.e. 00:00 the next day), are
+    // considered to be part of this day.
+    for (const event of events) {
+      const xStart = computeNumMinutes(event.start);
+      const xEnd = computeNumMinutes(event.end);
+      const width = xEnd - xStart;
+
+      const rect = createSvgElement("rect", {
+        x: xStart,
+        y: 0,
+        width: width,
+        height: BlockSchedule.#BLOCK_HEIGHT_MINUTES,
+        fill: stageColour,
+        stroke: BlockSchedule.#STROKE_COLOUR,
+        "stroke-width": BlockSchedule.#STROKE_WIDTH,
+      });
+      if (event.url) {
+        // If the event has a URL, add a click event to open the URL in a new
+        // tab.
+        rect.style.cursor = "pointer";
+        rect.addEventListener("click", () => window.open(event.url, "_blank"));
+      }
+      gBlocks.appendChild(rect);
+
+      // Create a foreign object with a div so we can more easily have nicely
+      // wrapping text, and smaller time text under the artist name.
+      const foreignObject = createSvgElement("foreignObject", {
+        x: xStart,
+        y: 0,
+        width: width,
+        height: BlockSchedule.#BLOCK_HEIGHT_MINUTES,
+      });
+      // Create wrapping flexbox div to layout the artist name and time.
+      const textContainerDiv = createXhtmlElement("div");
+      textContainerDiv.classList.add("block");
+
+      // Add the artist name and time to this div.
+      const nameDiv = createXhtmlElement("div");
+      nameDiv.textContent = event.name;
+      const timeDiv = createXhtmlElement("div");
+      timeDiv.classList.add("time");
+      timeDiv.textContent = `${event.start} – ${event.end}`;
+
+      textContainerDiv.appendChild(nameDiv);
+      textContainerDiv.appendChild(timeDiv);
+      foreignObject.appendChild(textContainerDiv);
+
+      gText.appendChild(foreignObject);
+    }
+
+    return new BlockSchedule(svg);
+  }
+}
+
+// Helper functions
+function createElement(tag, ns, attributes) {
+  const el = document.createElementNS(ns, tag);
+  if (attributes) {
+    for (const [key, value] of Object.entries(attributes)) {
+      el.setAttribute(key, value);
+    }
+  }
+  return el;
+}
+
+function createSvgElement(tag, attributes) {
+  return createElement(tag, "http://www.w3.org/2000/svg", attributes);
+}
+
+function createXhtmlElement(tag, attributes) {
+  return createElement(tag, "http://www.w3.org/1999/xhtml", attributes);
+}
+
+function computeNumMinutes(time) {
+  let [hours, minutes] = time.split(":").map((x) => parseInt(x));
+  if (hours < 12) {
+    hours += 24;
+  }
+  return hours * 60 + minutes;
 }
 
 async function main() {
@@ -57,10 +194,11 @@ function generateBlockSchedules(schedule) {
   for (const dayId of schedule.getDayIds()) {
     blockSchedules[dayId] = {};
     for (const stageId of schedule.getStageIds(dayId)) {
-      const events = schedule.getEvents(dayId, stageId);
-      const stage = schedule.getStage(stageId);
-      const svg = generateStageBlockSchedule(events, stage, "12:00", "04:00");
-      blockSchedules[dayId][stageId] = svg;
+      blockSchedules[dayId][stageId] = BlockSchedule.fromSchedule(
+        schedule,
+        dayId,
+        stageId,
+      );
     }
   }
   return blockSchedules;
@@ -80,81 +218,6 @@ function createSvgElement(tag, attributes) {
     el.setAttribute(key, value);
   }
   return el;
-}
-
-function generateStageBlockSchedule(
-  schedule,
-  stage,
-  scheduleStart,
-  scheduleEnd,
-) {
-  const startMinutes = parseTime(scheduleStart);
-  const endMinutes = parseTime(scheduleEnd);
-
-  const range = endMinutes - startMinutes;
-
-  const svg = createSvgElement("svg", {
-    viewBox: `${startMinutes} 0 ${range} ${blockHeightMinutes}`,
-    preserveAspectRatio: "none",
-  });
-  svg.setAttribute("height", blockHeight);
-  svg.setAttribute("width", (range / blockHeightMinutes) * blockHeight);
-
-  for (const event of schedule) {
-    const eventStartMinutes = parseTime(event.start);
-    const eventEndMinutes = parseTime(event.end);
-
-    const xStart = eventStartMinutes;
-    const xEnd = eventEndMinutes;
-    const width = xEnd - xStart;
-    const rect = createSvgElement("rect", {
-      x: xStart,
-      y: 0,
-      width: width,
-      height: blockHeightMinutes,
-      fill: stage.colour,
-      stroke: "white",
-      "stroke-width": 0.5,
-    });
-    rect.style.cursor = "pointer";
-    // If we have a URL, clicking the block navigate to the artist's page on
-    // the BKS website.
-    if (event.url) {
-      rect.addEventListener("click", () => window.open(event.url, "_blank"));
-    }
-    svg.appendChild(rect);
-
-    const foreignObject = createSvgElement("foreignObject", {
-      x: xStart,
-      y: 0,
-      width: width,
-      height: blockHeightMinutes,
-    });
-    const textContainerDiv = document.createElementNS(
-      "http://www.w3.org/1999/xhtml",
-      "div",
-    );
-    textContainerDiv.classList.add("block");
-    const nameDiv = document.createElementNS(
-      "http://www.w3.org/1999/xhtml",
-      "div",
-    );
-    nameDiv.textContent = event.name;
-    const timeDiv = document.createElementNS(
-      "http://www.w3.org/1999/xhtml",
-      "div",
-    );
-    timeDiv.classList.add("time");
-    timeDiv.textContent = `${event.start} – ${event.end}`;
-
-    textContainerDiv.appendChild(nameDiv);
-    textContainerDiv.appendChild(timeDiv);
-
-    foreignObject.appendChild(textContainerDiv);
-    svg.appendChild(foreignObject);
-  }
-
-  return svg;
 }
 
 async function fetchSchedule(url) {
@@ -251,7 +314,7 @@ function populateSchedule(schedule, blockSchedules, dayId, stageIds) {
 
   for (const stageId in blockSchedules) {
     if (!stageIds.includes(stageId)) continue;
-    const svg = blockSchedules[stageId];
+    const svg = blockSchedules[stageId].svg;
     svg.setAttribute(
       "viewBox",
       `${startMinutes} 0 ${range} ${blockHeightMinutes}`,
