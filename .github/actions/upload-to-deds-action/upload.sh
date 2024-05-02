6b9866ecfe3281ce187f27908a33e58a1225b841
Cees Voesenek <cjvoesenek@gmail.com>
2024-05-02 20:28:00 +0200

    Add some debug logging to upload.sh

--- .github/actions/upload-to-deds-action/upload.sh
+++ .github/actions/upload-to-deds-action/upload.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Username: ${INPUT_USERNAME}"
+echo "Password: ${INPUT_PASSWORD}"
 for file in ${INPUT_FILES}; do
+    echo "Uploading ${file} to ftp.deds.nl/www"
     ncftpput -u "${INPUT_USERNAME}" -p "${INPUT_PASSWORD}" ftp.deds.nl /www "${file}"
 done
