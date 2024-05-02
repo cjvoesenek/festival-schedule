c3b62eddbb2f161f4f57655f539a61dc65dcec6b
Cees Voesenek <cjvoesenek@gmail.com>
2024-05-02 20:17:42 +0200

    Use action inputs properly

--- .github/actions/upload-to-deds-action/upload.sh
+++ .github/actions/upload-to-deds-action/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for file in ${FILES}; do
-    ncftpput -u "${USERNAME}" -p "${PASSWORD}" ftp.deds.nl /www "${file}"
+for file in ${INPUT_FILES}; do
+    ncftpput -u "${INPUT_USERNAME}" -p "${INPUT_PASSWORD}" ftp.deds.nl /www "${file}"
 done
