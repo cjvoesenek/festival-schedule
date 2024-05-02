618d241a5bf5818a6cb5a0aa36ade15129a94875
Cees Voesenek <cjvoesenek@gmail.com>
2024-05-02 20:31:42 +0200

    Tweak logging

--- .github/actions/upload-to-deds-action/upload.sh
+++ .github/actions/upload-to-deds-action/upload.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo "Username: ${INPUT_USERNAME}"
-echo "Password: ${INPUT_PASSWORD}"
 for file in ${INPUT_FILES}; do
-    echo "Uploading ${file} to ftp.deds.nl/www"
+    echo "Uploading \"${file}\" to \"ftp.deds.nl/www/${file}\"..."
     ncftpput -u "${INPUT_USERNAME}" -p "${INPUT_PASSWORD}" ftp.deds.nl /www "${file}"
 done
