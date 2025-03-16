d0f1ee6676fd976cbc5d95d2b898bf7409b84310
Cees Voesenek <cjvoesenek@proton.me>
2025-03-16 15:40:33 +0100

    Fix uploading to DeDS

--- .github/actions/upload-to-deds-action/upload.sh
+++ .github/actions/upload-to-deds-action/upload.sh
@@ -2,5 +2,9 @@
 
 for file in ${INPUT_FILES}; do
     echo "Uploading \"${file}\"..."
-    ncftpput -R -u "${INPUT_USERNAME}" -p "${INPUT_PASSWORD}" ftp.deds.nl /www "${file}"
+    USERNAME=$(echo -n "$INPUT_USERNAME" | tr -d '\r' | tr -d '\n')
+    PASSWORD=$(echo -n "$INPUT_PASSWORD" | tr -d '\r' | tr -d '\n')
+    echo $(echo -n "${PASSWORD}" | wc -c)
+    ncftpls -m -R -u "${USERNAME}" -p "${PASSWORD}" ftp://ftp.deds.nl
+    ncftpput -m -R -u "${USERNAME}" -p "${PASSWORD}" ftp.deds.nl /www "${file}"
 done
