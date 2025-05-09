a6a170bd54774902177b60c8256f7f3fc4d2f13f
Cees Voesenek <cees.voesenek@vortech.nl>
2025-05-09 21:28:00 +0200

    Add DTRH 2025 schedule
    Also a Python script to parse it from their website.

---
+++ scripts/parse_dtrh2025.py
@@ -0,0 +1,227 @@
+import dataclasses
+import json
+import logging
+import pathlib
+import re
+import sys
+
+from bs4 import BeautifulSoup, Tag
+import requests
+
+logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class BlockHeightConfig:
+    coords: float
+    pixels: float
+
+
+@dataclasses.dataclass
+class ScheduleConfig:
+    blockHeight: BlockHeightConfig
+
+
+@dataclasses.dataclass
+class Stage:
+    id: str
+    name: str
+    colour: str
+
+
+@dataclasses.dataclass
+class Event:
+    start: str
+    end: str
+    name: str
+    url: str
+
+
+@dataclasses.dataclass
+class DaySchedule:
+    id: str
+    name: str
+    date: str
+    events: dict[str, list[Event]]
+
+
+@dataclasses.dataclass
+class Schedule:
+    config: ScheduleConfig
+    stages: list[Stage]
+    schedule: list[DaySchedule]
+
+
+@dataclasses.dataclass
+class Day:
+    id: str
+    dutch_name: str
+    name: str
+    date: str
+
+
+@dataclasses.dataclass
+class ParsedEvent:
+    day_id: str
+    stage_id: str
+    start: str
+    end: str
+    name: str
+    url: str
+
+
+def gather_events_from_artist_page(
+    name: str,
+    url: str,
+    stage_name_to_id: dict[str, str],
+    day_dutch_name_to_id: dict[str, str],
+) -> list[ParsedEvent]:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    metadata_div = soup.find("div", class_="border")
+    if metadata_div is None:
+        raise RuntimeError("Could not find metadata <div> element")
+
+    matches: list[str] = re.findall(
+        r"\w+,\s*\d{2}\s+Jul\s+2025\s+\d{2}:\d{2}\s+-\s+\d{2}:\d{2}\n[^\n]+",
+        metadata_div.text,
+    )
+    if len(matches) == 0:
+        raise RuntimeError(
+            "Could not find appropriately formatted metadata in metadata <div>"
+        )
+
+    events: list[ParsedEvent] = []
+    for match in matches:
+        raw_datetime, stage = match.split("\n")
+        stage_id = stage_name_to_id[stage]
+
+        day = raw_datetime.split(",")[0].lower()
+        day_id = day_dutch_name_to_id[day]
+
+        time_match = re.search(
+            r"(?P<start>\d{2}:\d{2}) - (?P<end>\d{2}:\d{2})", raw_datetime
+        )
+        if time_match is None:
+            raise ValueError("Failed to parse start and end date from metadata")
+        start = time_match.group("start")
+        end = time_match.group("end")
+
+        events.append(ParsedEvent(day_id, stage_id, start, end, name, url))
+    return events
+
+
+def gather_events(
+    main_url: str, days: list[Day], stages: list[Stage]
+) -> list[ParsedEvent]:
+    stage_name_to_id = {stage.name: stage.id for stage in stages}
+    day_dutch_name_to_id = {day.dutch_name: day.id for day in days}
+
+    response = requests.get(main_url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    artist_links = soup.find_all("a", class_="group")
+    num_events = len(artist_links)
+
+    events: list[ParsedEvent] = []
+    for i, link in enumerate(artist_links):
+        if not isinstance(link, Tag):
+            continue
+        name = link.attrs["title"]
+        url = link.attrs["href"]
+        if not isinstance(name, str) or not isinstance(url, str):
+            continue
+
+        logger.info(f"Gathering event {i + 1} of {num_events}: {name}...")
+        try:
+            events += gather_events_from_artist_page(
+                name, url, stage_name_to_id, day_dutch_name_to_id
+            )
+            logger.info("Success")
+        except Exception:
+            logger.exception("Failed")
+    return events
+
+
+def compute_sort_key_from_time(time: str) -> int:
+    # Compute the number of minutes past 12:00.
+    tokens = time.split(":")
+    if len(tokens) != 2:
+        raise RuntimeError("Failed to parse time string.")
+    hours = int(tokens[0])
+    minutes = int(tokens[1])
+    if hours < 12:
+        hours += 24
+    return (hours - 12) * 60 + minutes
+
+
+def convert_events_to_schedule(
+    parsed_events: list[ParsedEvent],
+    config: ScheduleConfig,
+    days: list[Day],
+    stages: list[Stage],
+) -> Schedule:
+    day_schedules: list[DaySchedule] = []
+    for day in days:
+        day_events: dict[str, list[Event]] = {}
+        for stage in stages:
+            # Find all events with the specified day and stage and sort by
+            # ascending start time.
+            events = sorted(
+                filter(
+                    lambda event: event.day_id == day.id and event.stage_id == stage.id,
+                    parsed_events,
+                ),
+                key=lambda event: compute_sort_key_from_time(event.start),
+            )
+            day_events[stage.id] = list(
+                map(
+                    lambda parsed: Event(
+                        parsed.start, parsed.end, parsed.name, parsed.url
+                    ),
+                    events,
+                )
+            )
+        day_schedules.append(DaySchedule(day.id, day.name, day.date, day_events))
+    return Schedule(config, stages, day_schedules)
+
+
+def main() -> None:
+    output_path = pathlib.Path("../schedules/dtrh2025.json")
+    log_path = pathlib.Path("dtrh2025.log")
+
+    logger.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.INFO)
+    logger.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler(log_path, mode="w", encoding="utf-8")
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+
+    main_url = "https://downtherabbithole.nl/programma"
+    config = ScheduleConfig(BlockHeightConfig(30, 100))
+    stages = [
+        Stage("hotot", "Hotot", "#1b9e77"),
+        Stage("teddy-widder", "Teddy Widder", "#d95f02"),
+        Stage("fuzzy-lop", "Fuzzy Lop", "#7570b3"),
+        Stage("rex", "REX", "#e7298a"),
+        Stage("bizarre", "The Bizarre", "#66a61e"),
+    ]
+    days = [
+        Day("friday", "vrijdag", "Friday", "2025-07-04"),
+        Day("saturday", "zaterdag", "Saturday", "2025-07-05"),
+        Day("sunday", "zondag", "Sunday", "2025-07-06"),
+    ]
+
+    parsed_events = gather_events(main_url, days, stages)
+    schedule = convert_events_to_schedule(parsed_events, config, days, stages)
+
+    with open(output_path, "w", encoding="utf-8") as file:
+        json.dump(dataclasses.asdict(schedule), file, indent=2)
+
+
+if __name__ == "__main__":
+    main()
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import "./main.css";
-import bks2024 from "../schedules/bks2024.json";
+import dtrh2025 from "../schedules/dtrh2025.json";
 
 import { App, Schedule } from "./lib";
 
 function main(): void {
-  const schedule = new Schedule(bks2024);
+  const schedule = new Schedule(dtrh2025);
 
   const daysContainer = document.querySelector("#days") as HTMLDivElement;
   const stagesContainer = document.querySelector("#stages") as HTMLDivElement;
