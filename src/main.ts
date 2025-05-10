e80cfb39b857afb37155e3ae673d7ad1ab0a3f95
Cees Voesenek <cees.voesenek@vortech.nl>
2025-05-10 20:42:17 +0200

    Show version (also links to source)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import "./main.css";
 import dtrh2025 from "../schedules/dtrh2025.json";
+import { version } from "../package.json";
 
 import { App, Schedule } from "./lib";
 
 function main(): void {
+  // Create schedule and application state manager.
   const schedule = new Schedule(dtrh2025);
 
   const daysContainer = document.querySelector("#days") as HTMLDivElement;
@@ -12,6 +14,12 @@ function main(): void {
   const nowButton = document.querySelector("#button-now") as HTMLDivElement;
 
   new App(daysContainer, stagesContainer, eventsContainer, nowButton, schedule);
+
+  // Set version string in label.
+  const versionLabel = document.querySelector(
+    "#version-label > a",
+  ) as HTMLAnchorElement;
+  versionLabel.textContent = version;
 }
 
 // Run the main function.
